6df2f584eb7d24fb27188d880daaafb147cd6cca
mdriyadmr968 <mdriyadmr968@gmail.com>
2022-05-09 20:13:12 +0600

    login and registration implemented

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,11 @@
 import { getAuth } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { useLocation, useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import app from "../../firebase.init";
+import './Login.css'
 
 const auth = getAuth(app);
 
@@ -10,6 +13,16 @@ const Login = () => {
   const [signInWithGoogle, user] = useSignInWithGoogle(auth);
   const location = useLocation();
   const navigate = useNavigate();
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+
+   function validateForm() {
+     return email.length > 0 && password.length > 0;
+   }
+
+   function handleSubmit(event) {
+     event.preventDefault();
+   }
 
    const from = location?.state?.from?.pathname || "/";
 
@@ -25,13 +38,30 @@ const Login = () => {
         <button onClick={handleGoogleSignIn}>Google Sign In</button>
       </div>
 
-      <form>
-        <input type="email" placeholder="Your Email" />
-        <br />
-        <input type="password" name="" id="" placeholder="Password" />
-        <br />
-        <input type="submit" value="Login" />
-      </form>
+      <div className="Login">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group size="lg" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              autoFocus
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group size="lg" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Button block size="lg" type="submit" disabled={!validateForm()}>
+            Login
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,30 +1,102 @@
 import React from 'react';
+import './Register.css'
+
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { getAuth } from 'firebase/auth';
+import app from '../../firebase.init';
+const auth = getAuth(app);
+
+
 
 const Register = () => {
+  const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  
+
+
+  const from = location?.state?.from?.pathname || "/";
+
+  const handleGoogleSignIn = () => {
+    signInWithGoogle().then(() => {
+      navigate(from, { replace: true });
+    });
+  };
     return (
       <div>
         <h2>register</h2>
-        <form onSubmit="">
-          <input type="email" name="" id="" placeholder="your email" />
-          <input
-            type="password"
-            name=""
-            id=""
-            placeholder="Enter your password"
-          />
-          <input
-            type="password"
-            name=""
-            id=""
-            placeholder="confirm your password"
-          />
-          <input type="submit" value="submit" />
-        </form>
+        <div className="form">
+          <div className="form-body">
+            <div className="username">
+              <label className="form__label" for="firstName">
+                First Name{" "}
+              </label>
+              <input
+                className="form__input"
+                type="text"
+                id="firstName"
+                placeholder="First Name"
+              />
+            </div>
+            <div className="lastname">
+              <label className="form__label" for="lastName">
+                Last Name{" "}
+              </label>
+              <input
+                type="text"
+                name=""
+                id="lastName"
+                className="form__input"
+                placeholder="LastName"
+              />
+            </div>
+            <div className="email">
+              <label className="form__label" for="email">
+                Email{" "}
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="form__input"
+                placeholder="Email"
+              />
+            </div>
+            <div className="password">
+              <label className="form__label" for="password">
+                Password{" "}
+              </label>
+              <input
+                className="form__input"
+                type="password"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="confirm-password">
+              <label className="form__label" for="confirmPassword">
+                Confirm Password{" "}
+              </label>
+              <input
+                className="form__input"
+                type="password"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+              />
+            </div>
+          </div>
+          <div class="footer">
+            <button type="submit" class="btn">
+              Register
+            </button>
+          </div>
+        </div>
         <p>
           already have an account? <a href="/login">Login</a>
         </p>
         <div>------------ or ------------</div>
-        <button className="btn-regular">Google Signin</button>
+        <button onClick={handleGoogleSignIn}>Google Signin</button>
       </div>
     );
 };
